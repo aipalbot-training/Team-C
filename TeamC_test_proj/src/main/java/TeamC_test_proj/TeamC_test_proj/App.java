503857770467334d8ecb98da43194834347dbf43
Adedayo-1 <66980214+Adedayo-1@users.noreply.github.com>
2020-07-01 23:50:27 -0400

    Default Project

---
+++ TeamC_test_proj/src/main/java/TeamC_test_proj/TeamC_test_proj/App.java
@@ -0,0 +1,13 @@
+package TeamC_test_proj.TeamC_test_proj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
