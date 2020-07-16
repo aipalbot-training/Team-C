49188449f41f95183d607d950849276fc3739f15
sontoly <sontoly@gmail.com>
2020-07-16 11:13:11 -0400

    default2

--- flight_reservation/src/main/java/com/ikenna/flight_reservation/App.java
+++ flight_reservation/src/main/java/com/ikenna/flight_reservation/App.java
@@ -1,5 +1,7 @@
 package com.ikenna.flight_reservation;
 
+import com.ikenna.flight_reservation.models.Person;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        Person p = new Person();
     }
 }
